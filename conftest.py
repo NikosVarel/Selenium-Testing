b3d5657ff85c7ff0bda2c83d7eb20d10e06b7aa3
NikosVarel <90154827+NikosVarel@users.noreply.github.com>
2021-09-15 07:08:56 +0300

    Update

--- conftest.py
+++ conftest.py
@@ -4,10 +4,6 @@ from selenium import webdriver
 def pytest_addoption(parser):
     parser.addoption("--driverName", action="store", default="Chrome")
 
-# @pytest.fixture(scope="class")
-# def driverName(request):
-#     return request.config.getoption("--driverName")
-
 @pytest.fixture(scope="class")
 def setup(request):
     driverName = request.config.getoption("--driverName")
@@ -20,10 +16,3 @@ def setup(request):
     request.cls.driver = driver
     yield
     driver.close()
-
-    # if driverName == "chrome":
-#         driver = webdriver.Chrome(executable_path="C:\\chromedriver.exe")
-#     elif driverName == "firefox":
-#         driver = webdriver.Chrome(executable_path="C:\\geckodriver.exe")
-#     elif driverName == "IE":
-#         driver = webdriver.Chrome(executable_path="C:\\IEDriverServer.exe")
