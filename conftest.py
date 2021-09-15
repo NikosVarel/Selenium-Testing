b6b65488281992c6ba698145b2129e69cb1221e1
NikosVarel <90154827+NikosVarel@users.noreply.github.com>
2021-09-15 06:39:49 +0300

    Update

--- conftest.py
+++ conftest.py
@@ -1,8 +1,23 @@
 import pytest
+from selenium import webdriver
 
-def pytest_addoption(parser):
-    parser.addoption("--driverName", action="store", default="chrome")
+# def pytest_addoption(parser):
+#     parser.addoption("--driverName", action="store", default="chrome")
+#
+# @pytest.fixture(scope="class")
+# def driverName(request):
+#     return request.config.getoption("--driverName")
 
-@pytest.fixture
-def driverName(request):
-    return request.config.getoption("--driverName")
+@pytest.fixture(scope="class")
+def setup(request):
+    driver = webdriver.Chrome(executable_path="C:\\chromedriver.exe")
+    request.cls.driver = driver
+    yield
+    driver.close()
+
+    # if driverName == "chrome":
+#         driver = webdriver.Chrome(executable_path="C:\\chromedriver.exe")
+#     elif driverName == "firefox":
+#         driver = webdriver.Chrome(executable_path="C:\\geckodriver.exe")
+#     elif driverName == "IE":
+#         driver = webdriver.Chrome(executable_path="C:\\IEDriverServer.exe")
--- test_main.py
+++ test_main.py
@@ -1,47 +1,36 @@
 import time
 import pytest
-from selenium import webdriver
 
-@pytest.mark.usefixture("drivername")
-class TestToolsQA():
+@pytest.mark.usefixtures("setup")
+class TestToolsQA:
 
-    def test_toolsqaform(self,driverName):
-        if driverName == "chrome":
-            driver = webdriver.Chrome(executable_path="C:\\chromedriver.exe")
-        elif driverName == "firefox":
-            driver = webdriver.Chrome(executable_path="C:\\geckodriver.exe")
-        elif driverName == "IE":
-            driver = webdriver.Chrome(executable_path="C:\\IEDriverServer.exe")
+    def test_toolsqaform(self,setup):
 
-        driver.get("https://demoqa.com/automation-practice-form")
-        driver.maximize_window()
-        driver.find_element_by_xpath("//input[@id='firstName']").send_keys("Nikos")
-        driver.find_element_by_xpath("//input[@id='lastName']").send_keys("Varelas")
-        radio_button = driver.find_elements_by_xpath("//*[@id='genterWrapper']/div[2]/div[1]/label")
+        self.driver.get("https://demoqa.com/automation-practice-form")
+        self.driver.maximize_window()
+
+        self.driver.find_element_by_xpath("//input[@id='firstName']").send_keys("Nikos")
+        self.driver.find_element_by_xpath("//input[@id='lastName']").send_keys("Varelas")
+        radio_button = self.driver.find_elements_by_xpath("//*[@id='genterWrapper']/div[2]/div[1]/label")
         radio_button[0].click()
-        driver.find_element_by_xpath("//input[@id='userNumber']").send_keys("6912345678")
-        driver.execute_script("window.scrollBy(0,1000)")
-        driver.find_element_by_xpath("//*[@id='submit']").click()
+        self.driver.find_element_by_xpath("//input[@id='userNumber']").send_keys("6912345678")
+        self.driver.execute_script("window.scrollBy(0,1000)")
+        self.driver.find_element_by_xpath("//*[@id='submit']").click()
         time.sleep(2)
-        submit_text = driver.find_element_by_xpath("//div[@id='example-modal-sizes-title-lg']").text
-        driver.get_screenshot_as_file("Sheets/ToolsQA_form.png")
+        submit_text = self.driver.find_element_by_xpath("//div[@id='example-modal-sizes-title-lg']").text
+        self.driver.get_screenshot_as_file("Sheets/ToolsQA_form.png")
         assert submit_text == "Thanks for submitting the form"
 
 
-    def test_toolsqabooks(self,driverName):
-        if driverName == "chrome":
-            driver = webdriver.Chrome(executable_path="C:\\chromedriver.exe")
-        elif driverName == "firefox":
-            driver = webdriver.Chrome(executable_path="C:\\geckodriver.exe")
-        elif driverName == "IE":
-            driver = webdriver.Chrome(executable_path="C:\\IEDriverServer.exe")
+    def test_toolsqabooks(self,setup):
 
-        driver.get("https://demoqa.com/login")
-        driver.maximize_window()
-        driver.find_element_by_xpath("//button[@id='newUser']").click()
-        driver.find_element_by_xpath("//input[@id='firstname']").send_keys("Nikos")
-        driver.find_element_by_xpath("//input[@id='lastname']").send_keys("Varelas")
-        driver.find_element_by_xpath("//input[@id='userName']").send_keys("NikosVarel")
-        driver.find_element_by_xpath("//input[@id='password']").send_keys("Nikos0!")
-        driver.get_screenshot_as_file("Sheets/ToolsQA_books.png")
+        self.driver.get("https://demoqa.com/login")
+        self.driver.maximize_window()
+
+        self.driver.find_element_by_xpath("//button[@id='newUser']").click()
+        self.driver.find_element_by_xpath("//input[@id='firstname']").send_keys("Nikos")
+        self.driver.find_element_by_xpath("//input[@id='lastname']").send_keys("Varelas")
+        self.driver.find_element_by_xpath("//input[@id='userName']").send_keys("NikosVarel")
+        self.driver.find_element_by_xpath("//input[@id='password']").send_keys("Nikos0!")
+        self.driver.get_screenshot_as_file("Sheets/ToolsQA_books.png")
 
