721352822582b2acf868fb68e6823d754b2ddce0
NikosVarel <90154827+NikosVarel@users.noreply.github.com>
2021-09-17 13:56:15 +0300

    Logging Update

--- tests/test_toolsqaform.py
+++ tests/test_toolsqaform.py
@@ -1,5 +1,13 @@
-class UnitTestForm:
+import logging
+
+class UnitTestForm():
     def test_toolsqaform(self):
+        logger = logging.getLogger(__name__)
+        fileHandler = logging.FileHandler('Sheets/Log files/logfile.log')
+        formatter = logging.Formatter("%(asctime)s :%(levelname)s : %(name)s :%(message)s")
+        fileHandler.setFormatter(formatter)
+        logger.addHandler(fileHandler)  # filehandler object
+        logger.setLevel(logging.DEBUG)
 
         self.driver.get("https://demoqa.com/automation-practice-form")
         self.driver.maximize_window()
@@ -13,4 +21,5 @@ class UnitTestForm:
         self.driver.find_element_by_xpath("//*[@id='submit']").click()
         submit_text = self.driver.find_element_by_xpath("//div[@id='example-modal-sizes-title-lg']").text
         self.driver.get_screenshot_as_file("Sheets/ToolsQA_form.png")
+        logger.info(submit_text)
         assert submit_text == "Thanks for submitting the form"
--- utilities/BaseClass.py
+++ utilities/BaseClass.py
@@ -1,10 +1,11 @@
+import logging
 import pytest
 
 from tests.test_toolsqabooks import UnitTestBooks
 from tests.test_toolsqaform import UnitTestForm
 
-
 @pytest.mark.usefixtures("setup")
 class BaseClass(UnitTestForm,UnitTestBooks):
+
     UnitTestForm()
     UnitTestBooks()
